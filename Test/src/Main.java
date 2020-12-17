57ad238767f6a88131cb1eded098fb91fc4327e3
ChengYi Tang <Thomas Tang>
2020-12-17 11:43:48 +0800

    12/17/01

---
+++ Test/src/Main.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		//User Input
+		System.out.println("Enter Keyword: ");
+		Scanner sc = new Scanner(System.in);
+		String keyword = sc.next();
+		
+		// fetch URL on google
+		try {
+			System.out.println(new GoogleQuery(keyword).query());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*
+		// word counter
+		System.out.println("Please type: URL Keyword");
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextLine())
+		{
+		    String urlStr = sc.next();
+		    String keyword = sc.next();
+		   
+		    WordCounter counter = new WordCounter(urlStr);
+		    System.out.println(counter.countKeyword(keyword));//times keyword appears
+		}*/
+	
+	}}
